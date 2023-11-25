2f115c7d6203e2a2a793244ba3e970cf15b73dba
LayneHansen <layne.d.hansen@gmail.com>
2023-11-24 20:23:23 -0500

    added categories to explorer

---
+++ src/components/ListingItem.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom'
+import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
+import bedIcon from '../assets/svg/bedIcon.svg'
+import bathtubIcon from '../assets/svg/bathtubIcon.svg'
+
+function Listing({ listing, id }) {
+  console.log(listing)
+  return (
+    <li className="categoryListing">
+      <Link to={`/category/${listing.type}/${id}`}
+        className='categoryListingLink'
+        >
+        <img src={listing.imgUrls[0]} 
+          alt={listing.name} 
+          className="categoryListingImg" />
+      </Link>
+    </li>
+  )
+}
+
+export default Listing
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,9 +1,35 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import { Link } from 'react-router-dom'
+import rentCategoryImage from '../assets/jpg/rentCategoryImage.jpg'
+import sellCategoryImage from '../assets/jpg/sellCategoryImage.jpg'
+
+
 function Explore() {
   return (
-    <div>
-      <h1>
-        Explore
-      </h1>
+    <div className='explore'>
+      <header>
+        <p className="pageHeader"></p>
+      </header>
+
+      <main>
+        <p className="exploreCategoryHeading">
+          Categories
+        </p>
+        <div className="exploreCategories">
+          <Link to='/category/rent'>
+            <img src={rentCategoryImage} 
+            alt="rent image" 
+            className='exploreCategoryImg'/>
+          <p className="exploreCategoryName">Places for Rent</p>
+          </Link>
+          <Link to='/category/sale'>
+            <img src={sellCategoryImage} 
+            alt="sell image" 
+            className='exploreCategoryImg'/>
+          <p className="exploreCategoryName">Places for Sale</p>
+          </Link>
+        </div>
+      </main>
     </div>
   )
 }
