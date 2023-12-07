44d8b49be6694400b1dff9acf00bd2483dc452e6
LayneHansen <layne.d.hansen@gmail.com>
2023-12-06 21:27:14 -0500

    updated api key

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -83,8 +83,7 @@ function CreateListing() {
 
     if(geolocationEnabled) {
       const response = await fetch(
-        //`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyC6poeCPQdahNNHh7oCaflBxzha6fUPyOI`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       )
 
         const data = await response.json()
