004f35db54eb6aae531bd7cc9685f8cbb9b18298
LayneHansen <layne.d.hansen@gmail.com>
2023-12-14 21:14:38 -0500

    added contact page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -99,9 +99,7 @@ function Listing() {
           {/* MAP */}
 
           {auth.currentUser?.uid !== listing.userRef && (
-            <Link to={`/contact/${listing.userRef}?listingName=
-              ${listing.name}&listingLocation=
-              ${listing.location}`} 
+            <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} 
               className='primaryButton'>
               Contact Landlord
             </Link>
